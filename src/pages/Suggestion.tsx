f6764fd2b0c9a0678aa9d5ca36d2721327c5f946
Ubinquitous <ubinquitous1@gmail.com>
2024-01-08 22:01:47 +0900

    feat : suggestion list page publish

---
+++ src/pages/Suggestion.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import SuggestionLayout from "../components/ui/Suggestion";
+
+const Suggestion = () => {
+  return <SuggestionLayout />;
+};
+
+export default Suggestion;
