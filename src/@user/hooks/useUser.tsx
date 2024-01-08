4f629465feb774980a66cdc82a4efce8359d5371
Ubinquitous <ubinquitous1@gmail.com>
2024-01-08 22:34:29 +0900

    feat : useUser hooks

---
+++ src/@user/hooks/useUser.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import { isAxiosError } from "axios";
+import { useAtom } from "jotai";
+import { User } from "../types";
+import { userContext } from "../context";
+import { ROLE } from "../constants";
+import { TOKEN } from "../../constants/token.constant";
+import ERROR from "../../constants/error.constant";
+import refresh from "../../apis/refresh";
+import { instance } from "../../apis/instance";
+
+const useUser = () => {
+  const [user, setUser] = useAtom(userContext);
+
+  const {
+    data: userInfo,
+    error,
+    refetch,
+  } = useQuery<User>(
+    ["useUser"],
+    async () => {
+      const { data } = await instance.post("");
+      return data;
+    },
+    { enabled: !!localStorage.getItem(TOKEN.ACCESS) }
+  );
+
+  const isSameUser = (id: number) => {
+    return userInfo?.id === id;
+  };
+
+  React.useEffect(() => {
+    if (isAxiosError(error) && error.response) {
+      const { code } = error.response.data;
+      if (code === ERROR.CODE.TOKEN_403_2) {
+        refresh().then(() => refetch());
+      }
+    }
+  }, [error, refetch]);
+
+  React.useEffect(() => {
+    if (userInfo) setUser(userInfo);
+  }, [setUser, userInfo]);
+
+  return {
+    user,
+    isLoggedIn: !!userInfo,
+    isAdmin: userInfo?.authority === ROLE.ADMIN,
+    role: userInfo?.role === "STUDENT" ? "학생" : "선생님",
+    isSameUser,
+  };
+};
+
+export default useUser;
